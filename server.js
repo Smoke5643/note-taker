379fbe133d7a95b53f1eac58b2266bedfa6beb6c
Smoke5643 <smoke5643@gmail.com>
2023-01-05 16:20:42 -0500

    added route in server.js

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const path = require('path');
+const uuid = require('./helpers/uuid');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
@@ -11,5 +16,9 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, './public/notes.html'))
 );
+
+// app.get('/api/notes', (req, res) =>
+//    fs.readfile()
+// );
 
 app.listen(PORT);
