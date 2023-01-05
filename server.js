831091711b8e449820ae25d82a5bec3332ee99b2
Smoke5643 <smoke5643@gmail.com>
2023-01-05 13:47:05 -0500

    added app.get(s) to land on homepage

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Note Taker');
-});
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+);
 
 app.listen(PORT);
