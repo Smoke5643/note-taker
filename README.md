2ffa49f734606175425ba0677158bcc3d0f62f44
Smoke5643 <smoke5643@gmail.com>
2023-01-06 15:26:28 -0500

    cleaned up code and added notes. also added README

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# note-taker
+# Note Taker Application
+
+[Link to my project](https://pacific-wildwood-35009.herokuapp.com/)
+
+## Table of Contents
+- [Description](#description-of-project)
+- [Questions](#questions)
+- [Visuals](#visuals)
+
+## Description of project
+
+
+This application was created to demonstrate how we can leverage the Express module in Node to run a dynamic web application. When the title and body of a note is added, and the save icon is clicked, it will persist in the left hand column. From here, you can click on that saved note at any time to redisplay it. Now we can all keep track of our notes and stay organized!
+
+## Questions
+  
+  If you have any questions about this project, please feel free to email me at smoke5643@gmail.com or on Github at https://www.github.com/Smoke5643. I will do my best to get back to you as soon as possible!
+
+## Visuals
+
+Preview of Application
+
+![image](public/assets/images/screenshot.png)
+
--- server.js
+++ server.js
@@ -1,49 +1,47 @@
+// Bringing in all required dependencies
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const uuid = require('./helpers/uuid');
+// Setting port for Heroku and local use
 const PORT = process.env.PORT || 3001;
-
+// Setting variable to call express funtion
 const app = express();
-
+// Allow express to take in form and JSON data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Giving access to pulic folder
 app.use(express.static('public'));
-
+// Variable to write notes to file
 const writeFile = (destination, content) =>
-  fs.writeFile(destination, JSON.stringify(content, null, 2), (err) =>
-    err ? console.error(err) : console.info(`\nNote written to ${destination}`)
-  );
-
+  fs.writeFileSync(destination, JSON.stringify(content, null, 2));
+// Variable to read and update file with new note
 const appendFile = (content, file) => {
-  fs.readFile(file, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      const parsedNote = JSON.parse(data);
-      parsedNote.push(content);
-      writeFile(file, parsedNote);
-    }
-  });
+  const data = fs.readFileSync(file, 'utf8')
+  if (data) {
+    const parsedNote = JSON.parse(data);
+    parsedNote.push(content);
+    writeFile(file, parsedNote);
+  };
 };
-
+// Serving the main homepage
 app.get('/', (req, res) => {
   console.info(`${req.method} request recieved!`);
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
-
+// Serving the notes page
 app.get('/notes', (req, res) => {
   console.info(`${req.method} request recieved!`);
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
-
+// Getting notes from db.json
 app.get('/api/notes', (req, res) => {
   console.info(`${req.method} request recieved!`);
   const data = fs.readFileSync('./db/db.json', 'utf8');
   res.json(JSON.parse(data));
 });
-
-app.post('/api/notes', function (req, res) {
+// Adding notes to db.json
+app.post('/api/notes', (req, res) => {
   console.info(`${req.method} request recieved!`);
   const { title, text } = req.body;
 
@@ -60,5 +58,5 @@ app.post('/api/notes', function (req, res) {
     res.error('There was an error adding the note!')
   }
 });
-
+// Listening at the specified port to run application
 app.listen(PORT);
