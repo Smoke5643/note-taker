ab8a0d68f8de30b771814fbf7ce0347b99cf398c
Smoke5643 <smoke5643@gmail.com>
2023-01-05 20:27:15 -0500

    started adding logic for POST for api/notes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const uuid = require('./helpers/uuid');
 const PORT = process.env.PORT || 3001;
 
@@ -17,8 +18,35 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
-// app.get('/api/notes', (req, res) =>
-//    fs.readfile()
-// );
+app.get('/api/notes', (req, res) =>
+   res.json(notes)
+);
+
+app.post("/api/notes", function(req, res) {
+  const { title, text } = req.body
+
+  if(title && text){
+  const newNote = {
+    title,
+    text,
+    id: uuid(),
+  };
+
+  const readNotes = fs.readFileSync(`./db/db.json`, 'utf8');
+  const parsedNotes = JSON.parse(readNotes);
+
+  parsedNotes.push(newNote);
+
+  const noteString = JSON.stringify(parsedNotes)
+
+  fs.writeFile(`./db/db.json`, noteString, (err) =>
+      err
+        ? console.error(err)
+        : console.log(
+            `New note has been written to JSON file`
+          )
+    );
+  };
+});
 
 app.listen(PORT);
