c4afdfe6db5fccca4d986ec59f455fac871d4b1e
Smoke5643 <smoke5643@gmail.com>
2023-01-05 21:49:16 -0500

    got read and write working, cleaning up formatting

--- server.js
+++ server.js
@@ -10,25 +10,40 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/index.html'))
-);
+const writeFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 2), (err) =>
+    err ? console.error(err) : console.info(`\nNote written to ${destination}`)
+  );
 
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/notes.html'))
-);
-
-fs.readFile('db/db.json', 'utf-8', (err, data) => {
-  if (err) throw err;
-  const savedNotes = JSON.parse(data)
-
-  app.get('/api/notes', (req, res) => {
-    res.json(savedNotes)
+const appendFile = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedNote = JSON.parse(data);
+      parsedNote.push(content);
+      writeFile(file, parsedNote);
+    }
   });
-})
+};
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  console.info(`${req.method} request recieved!`);
+  const data = fs.readFileSync('./db/db.json', 'utf8');
+  res.json(JSON.parse(data));
+});
 
 app.post("/api/notes", function (req, res) {
-  const { title, text } = req.body
+  console.info(`${req.method} request recieved!`);
+  const { title, text } = req.body;
 
   if (title && text) {
     const newNote = {
@@ -37,21 +52,11 @@ app.post("/api/notes", function (req, res) {
       id: uuid(),
     };
 
-    const readNotes = fs.readFileSync(`./db/db.json`, 'utf8');
-    const parsedNotes = JSON.parse(readNotes);
-
-    parsedNotes.push(newNote);
-
-    const noteString = JSON.stringify(parsedNotes, null, 2)
-
-    fs.writeFile(`./db/db.json`, noteString, (err) =>
-      err
-        ? console.error(err)
-        : console.log(
-          `New note has been written to JSON file`
-        )
-    );
-  };
+    appendFile(newNote, './db/db.json');
+    res.json('The new note had been added successfully.')
+  } else {
+    res.error('There was an error adding the note!')
+  }
 });
 
 app.listen(PORT);
