1d46fe159458d0f7614d2b0ca6e77785bc94f429
Smoke5643 <smoke5643@gmail.com>
2023-01-05 21:39:28 -0500

    changed read and write in const to be used in get and post

--- server.js
+++ server.js
@@ -18,33 +18,38 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
-app.get('/api/notes', (req, res) =>
-   res.json(notes)
-);
+fs.readFile('db/db.json', 'utf-8', (err, data) => {
+  if (err) throw err;
+  const savedNotes = JSON.parse(data)
 
-app.post("/api/notes", function(req, res) {
+  app.get('/api/notes', (req, res) => {
+    res.json(savedNotes)
+  });
+})
+
+app.post("/api/notes", function (req, res) {
   const { title, text } = req.body
 
-  if(title && text){
-  const newNote = {
-    title,
-    text,
-    id: uuid(),
-  };
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
 
-  const readNotes = fs.readFileSync(`./db/db.json`, 'utf8');
-  const parsedNotes = JSON.parse(readNotes);
+    const readNotes = fs.readFileSync(`./db/db.json`, 'utf8');
+    const parsedNotes = JSON.parse(readNotes);
 
-  parsedNotes.push(newNote);
+    parsedNotes.push(newNote);
 
-  const noteString = JSON.stringify(parsedNotes)
+    const noteString = JSON.stringify(parsedNotes, null, 2)
 
-  fs.writeFile(`./db/db.json`, noteString, (err) =>
+    fs.writeFile(`./db/db.json`, noteString, (err) =>
       err
         ? console.error(err)
         : console.log(
-            `New note has been written to JSON file`
-          )
+          `New note has been written to JSON file`
+        )
     );
   };
 });
